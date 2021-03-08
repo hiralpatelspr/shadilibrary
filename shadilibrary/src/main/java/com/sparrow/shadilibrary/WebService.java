f9bb93260c50770c833daaba63d787fd33ad6420
pooja gadani <mail2pooja.gadani@gmail.com>
2021-03-08 17:15:13 +0530

    new

--- shadilibrary/src/main/java/com/sparrow/shadilibrary/WebService.java
+++ shadilibrary/src/main/java/com/sparrow/shadilibrary/WebService.java
@@ -1,4 +1,40 @@
 package com.sparrow.shadilibrary;
 
+import com.google.gson.Gson;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class WebService {
+    private static WebServiceInterface webApiInterface;
+
+
+    public static WebServiceInterface getClient() {
+        if (webApiInterface == null) {
+
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.level(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient okclient = new OkHttpClient.Builder()
+                    .addInterceptor(logging)
+                    .connectTimeout(100, TimeUnit.SECONDS)
+                    .readTimeout(100, TimeUnit.SECONDS)
+                    .writeTimeout(100, TimeUnit.SECONDS)
+                    .build();
+            Retrofit client = new Retrofit.Builder()
+                    .baseUrl("http://gohelvijay47932685.ipage.com/mrg_lib/")
+                    .client(okclient)
+                    .addConverterFactory(GsonConverterFactory.create(new Gson()))
+                    .build();
+            webApiInterface = client.create(WebServiceInterface.class);
+        }
+        return webApiInterface;
+    }
+
+    public interface WebServiceInterface {
+
+    }
 }
