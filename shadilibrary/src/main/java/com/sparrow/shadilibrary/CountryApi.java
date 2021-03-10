b34638bc6a283548dec45e787e6a891df9a0293e
VIRAJ <viraj12edu@gmail.com>
2021-03-10 16:27:45 +0530

    new

--- shadilibrary/src/main/java/com/sparrow/shadilibrary/CountryApi.java
+++ shadilibrary/src/main/java/com/sparrow/shadilibrary/CountryApi.java
@@ -1,4 +1,29 @@
 package com.sparrow.shadilibrary;
 
+import android.content.Context;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class CountryApi {
+
+    public static void contry(Context context, final List<String> countries){
+        WebService.getClient().country().enqueue(new Callback<country>() {
+            @Override
+            public void onResponse(Call<country> call, Response<country> response) {
+                for(int i = 0;i<=response.body().getDetail().size();i++){
+                countries.add(response.body().getDetail().get(i).getName());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<country> call, Throwable t) {
+
+            }
+        });
+
+    }
 }
