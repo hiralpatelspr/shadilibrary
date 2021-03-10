2320755c26c1bd3279c9d3d53c09f87e85f5a29a
VIRAJ <viraj12edu@gmail.com>
2021-03-10 17:54:35 +0530

    it1

--- shadilibrary/src/main/java/com/sparrow/shadilibrary/CountryApi.java
+++ shadilibrary/src/main/java/com/sparrow/shadilibrary/CountryApi.java
@@ -1,6 +1,8 @@
 package com.sparrow.shadilibrary;
 
 import android.content.Context;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import java.util.List;
 
@@ -10,13 +12,18 @@ import retrofit2.Response;
 
 public class CountryApi {
 
-    public static void contry(Context context, final List<String> countries){
+    public static void contry(final Context context, final List<String> countries, final Spinner spinner){
         WebService.getClient().country().enqueue(new Callback<country>() {
             @Override
             public void onResponse(Call<country> call, Response<country> response) {
                 for(int i = 0;i< response.body().getDetail().size();i++){
                 countries.add(response.body().getDetail().get(i).getName());
                 }
+
+                ArrayAdapter<String> adp1 = new ArrayAdapter<String>(context,
+                        android.R.layout.simple_list_item_1, countries);
+                adp1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setAdapter(adp1);
             }
 
             @Override
