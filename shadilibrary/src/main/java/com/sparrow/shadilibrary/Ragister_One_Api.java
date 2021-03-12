40de6c072fa296e3ba5ede154e6b583ebe585c04
VIRAJ <viraj12edu@gmail.com>
2021-03-12 11:41:29 +0530

    it

--- shadilibrary/src/main/java/com/sparrow/shadilibrary/Ragister_One_Api.java
+++ shadilibrary/src/main/java/com/sparrow/shadilibrary/Ragister_One_Api.java
@@ -11,17 +11,22 @@ public class Ragister_One_Api {
 
     public static void ragisterone(final Context context, String name, String mobile, String gender, String email, String looking, String country, String dob, String password){
 
-        WebService.getClient().ragistert1(name, mobile, gender, email, looking, country, dob, password).enqueue(new Callback<Ragister_one>() {
-            @Override
-            public void onResponse(Call<Ragister_one> call, Response<Ragister_one> response) {
-                Toast.makeText(context, ""+response.body().getMessage(), Toast.LENGTH_SHORT).show();
-            }
 
-            @Override
-            public void onFailure(Call<Ragister_one> call, Throwable t) {
+        if(name.equals("")&&mobile.equals("")&&gender.equals("")&&email.equals("")&&looking.equals("")&&country.equals("")&&dob.equals("")&&password.equals("")){
+            Toast.makeText(context, "Must be fill all field!!!", Toast.LENGTH_SHORT).show();
+        }else {
+            WebService.getClient().ragistert1(name, mobile, gender, email, looking, country, dob, password).enqueue(new Callback<Ragister_one>() {
+                @Override
+                public void onResponse(Call<Ragister_one> call, Response<Ragister_one> response) {
+                    Toast.makeText(context, "" + response.body().getMessage(), Toast.LENGTH_SHORT).show();
+                }
 
-            }
-        });
+                @Override
+                public void onFailure(Call<Ragister_one> call, Throwable t) {
+
+                }
+            });
+        }
 
     }
 }
