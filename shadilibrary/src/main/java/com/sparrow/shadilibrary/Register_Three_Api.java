be61e7414b4e38d2c7d1645d9a4d9b37d93dafa4
hiralpatelspr <hiral1234567>
2021-03-18 16:37:46 +0530

    itnew

--- shadilibrary/src/main/java/com/sparrow/shadilibrary/Register_Three_Api.java
+++ shadilibrary/src/main/java/com/sparrow/shadilibrary/Register_Three_Api.java
@@ -11,10 +11,10 @@ import retrofit2.Response;
 
 public class Register_Three_Api {
 
-    public static void registerThreeApi(final Context context,String mStatus, String mobile, String height,String weight,
+    public static void registerThreeApi(final Context context, String mobile,String mstatus, String height,String weight,
                                         String living,String birth,String nationality){
 
-        WebService.getClient().ragistert3(mobile, mStatus, height, weight, living, birth, nationality).enqueue(new Callback<Registe_three>() {
+        WebService.getClient().ragistert3(mobile, mstatus, height, weight, living, birth, nationality).enqueue(new Callback<Registe_three>() {
             @Override
             public void onResponse(@NotNull Call<Registe_three> call, @NotNull Response<Registe_three> response) {
                 assert response.body() != null;
